75f76679985b8e1db071505f89035273ae78cbd5
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-12-07 02:30:58 +0200

    Minor changes

--- feedback-system/routes/users.js
+++ feedback-system/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET usersRoute listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
